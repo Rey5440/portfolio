23a51e517862d67d8f38c98ee20df520a1a69605
F.Fernandez <hellsingsd@gmail.com>
2023-10-14 23:00:49 -0300

    arrancando el portfolio

---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../nav/nav";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Nav/>
+            <h1>este es el home</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,11 @@
+
+const Nav = () => {
+
+    return (
+        <div>
+            <h1>este es el nav</h1>
+        </div>
+    )
+}
+
+export default Nav;
